1d28e0218b567c4104ad482edbffc68e0e581aeb
christian <chg60@pitt.edu>
2019-05-15 10:42:12 -0400

    modified functions and improved docstrings

--- tools/scripts/misc_functions.py
+++ tools/scripts/misc_functions.py
@@ -23,6 +23,14 @@ def validate_mysql_credentials(username, password):
 
 
 def get_database_names(username, password):
+	"""
+	Connects to MySQL using verified username and password and queries
+	for all databases the user has access to.
+	:param username: verified MySQL username
+	:param password: verified MySQL password
+	:return databases: list of MySQL databases that the verified user
+	has access to.
+	"""
 	try:
 		databases = []
 		connection = pms.connect("localhost", username, password)
@@ -39,8 +47,16 @@ def get_database_names(username, password):
 
 
 def get_database_hosts(username, password, database):
+	"""
+	Connects to MySQL using verified username, password, and a database
+	and queries for all the hosts in that database.
+	:param username: verified MySQL username
+	:param password: verified MySQL password
+	:param database: selected MySQL database
+	:return hosts: list of hosts in the selected database
+	"""
 	try:
-		hosts = []
+		hosts = list()
 		connection = pms.connect("localhost", username, password, database)
 		cursor = connection.cursor()
 		cursor.execute("SELECT DISTINCT HostStrain FROM phage")
@@ -54,6 +70,14 @@ def get_database_hosts(username, password, database):
 
 
 def get_database_clusters(username, password, database):
+	"""
+	Connects to MySQL using verified username, password, and a database
+	and queries for all the clusters in that database.
+	:param username: verified MySQL username
+	:param password: verified MySQL password
+	:param database: selected MySQL database
+	:return clusters: list of clusters in the selected database
+	"""
 	try:
 		clusters = list()
 		connection = pms.connect("localhost", username, password, database)
@@ -73,71 +97,182 @@ def get_database_clusters(username, password, database):
 		return
 
 
-def get_database_phages(username, password, database, host=None, cluster=None):
-	if host:
+def get_phams_by_host(username, password, database, host, status):
+	"""
+	Connects to MySQL using verified username, password, and a database
+	and queries for all phages and phams in the database matching the
+	input host and status.
+	:param username: verified MySQL username
+	:param password: verified MySQL password
+	:param database: selected MySQL database
+	:param host: selected genome HostStrain
+	:param status: selected genome status
+	:return pham_data: dictionary whose keys are phageids and whose
+	values are the phams in that phageid.
+	"""
+	if status is None or status == 0:
 		try:
-			phages = list()
-			connection = pms.connect("localhost", username, password, database)
-			cursor = connection.cursor()
-			cursor.execute("SELECT PhageID FROM phage WHERE HostStrain = "
-						   "'{}'".format(host))
-			results = cursor.fetchall()
+			pham_data = {}
+			query = "SELECT * FROM (SELECT a.PhageID, c.name, a.status FROM " \
+					"phage AS a INNER JOIN gene AS b ON a.PhageID = " \
+					"b.PhageID INNER JOIN pham AS c ON b.GeneID = c.GeneID " \
+					"WHERE a.HostStrain = '{}') AS d ORDER BY " \
+					"d.PhageID ASC".format(host)
+			con = pms.connect("localhost", username, password, database)
+			cur = con.cursor()
+			cur.execute(query)
+			results = cur.fetchall()
+			con.close()
 			for result in results:
-				phages.append(result[0])
-			connection.close()
-			return phages
-		except pms.err.Error:
-			return
-	elif cluster:
-		try:
-			phages = list()
-			connection = pms.connect("localhost", username, password, database)
-			cursor = connection.cursor()
-			if cluster == "Singleton":
-				cursor.execute("SELECT PhageID FROM phage WHERE Cluster is "
-							   "Null")
-			elif cluster == "Unclustered":
-				cursor.execute("SELECT PhageID FROM phage WHERE Cluster = "
-							   "'UNK'")
-			else:
-				cursor.execute("SELECT PhageID FROM phage WHERE Cluster = "
-						   "'{}'".format(cluster))
-			results = cursor.fetchall()
-			for result in results:
-				phages.append(result[0])
-			connection.close()
-			return phages
+				phageid = result[0]
+				phams = pham_data.get(phageid, [])
+				phams.append(result[1])
+				pham_data[phageid] = phams
+			return pham_data
 		except pms.err.Error as err:
 			print(err)
 			return
-	else:
+	elif status == 1:
 		try:
-			phages = list()
-			connection = pms.connect("localhost", username, password, database)
-			cursor = connection.cursor()
-			cursor.execute("SELECT PhageID FROM phage")
-			results = cursor.fetchall()
+			pham_data = {}
+			query = "SELECT * FROM (SELECT a.PhageID, c.name, a.status FROM " \
+					"phage AS a INNER JOIN gene AS b ON a.PhageID = b.PhageID" \
+					" INNER JOIN pham AS c ON b.GeneID = c.GeneID WHERE " \
+					"a.HostStrain = '{}' AND a.status = 'final') AS d ORDER " \
+					"BY d.PhageID ASC".format(host)
+			con = pms.connect("localhost", username, password, database)
+			cur = con.cursor()
+			cur.execute(query)
+			results = cur.fetchall()
+			con.close()
 			for result in results:
-				phages.append(result[0])
-			connection.close()
-			return phages
-		except pms.err.Error:
+				phageid = result[0]
+				phams = pham_data.get(phageid, [])
+				phams.append(result[1])
+				pham_data[phageid] = phams
+			return pham_data
+		except pms.err.Error as err:
+			print(err)
 			return
 
 
-def get_phams(username, password, database, phage):
+def get_phams_by_cluster(username, password, database, cluster, status):
+	"""
+	Connects to MySQL using verified username, password, and a database
+	and queries for all phages and phams in the database matching the
+	input cluster and status.
+	:param username: verified MySQL username
+	:param password: verified MySQL password
+	:param database: selected MySQL database
+	:param cluster: selected genome Cluster
+	:param status: selected genome status
+	:return pham_data: dictionary whose keys are phageids and whose
+	values are the phams in that phageid.
+	"""
+	if status is None or status == 0:
+		try:
+			pham_data = {}
+			# Format the query based on cluster; status is irrelevant
+			if cluster == "Singleton":
+				query ="SELECT * FROM (SELECT a.PhageID, c.name, a.status " \
+						"FROM phage AS a INNER JOIN gene AS b ON a.PhageID " \
+						"= b.PhageID INNER JOIN pham AS c ON b.GeneID = " \
+						"c.GeneID WHERE a.Cluster is Null) AS d ORDER BY " \
+						"d.PhageID ASC".format(cluster)
+			elif cluster == "Unclustered":
+				query = "SELECT * FROM (SELECT a.PhageID, c.name, a.status " \
+						"FROM phage AS a INNER JOIN gene AS b ON a.PhageID " \
+						"= b.PhageID INNER JOIN pham AS c ON b.GeneID = " \
+						"c.GeneID WHERE a.Cluster = 'UNK') AS d ORDER BY " \
+						"d.PhageID ASC".format(cluster)
+			else:
+				query = "SELECT * FROM (SELECT a.PhageID, c.name, a.status " \
+						"FROM phage AS a INNER JOIN gene AS b ON a.PhageID " \
+						"= b.PhageID INNER JOIN pham AS c ON b.GeneID = " \
+						"c.GeneID WHERE a.Cluster = '{}') AS d ORDER BY " \
+						"d.PhageID ASC".format(cluster)
+			con = pms.connect("localhost", username, password, database)
+			cur = con.cursor()
+			cur.execute(query)
+			results = cur.fetchall()
+			con.close()
+			for result in results:
+				phageid = result[0]
+				phams = pham_data.get(phageid, [])
+				phams.append(result[1])
+				pham_data[phageid] = phams
+			return pham_data
+		except pms.err.Error as err:
+			print(err)
+			return
+	elif status == 1:
+		try:
+			pham_data = {}
+			# Format the query based on cluster and status=final
+			if cluster == "Singleton":
+				query ="SELECT * FROM (SELECT a.PhageID, c.name, a.status " \
+						"FROM phage AS a INNER JOIN gene AS b ON a.PhageID " \
+						"= b.PhageID INNER JOIN pham AS c ON b.GeneID = " \
+						"c.GeneID WHERE a.Cluster is Null AND a.status = " \
+						"'final') AS d ORDER BY d.PhageID ASC".format(cluster)
+			elif cluster == "Unclustered":
+				query = "SELECT * FROM (SELECT a.PhageID, c.name, a.status " \
+						"FROM phage AS a INNER JOIN gene AS b ON a.PhageID " \
+						"= b.PhageID INNER JOIN pham AS c ON b.GeneID = " \
+						"c.GeneID WHERE a.Cluster = 'UNK' AND a.status = " \
+						"'final') AS d ORDER BY d.PhageID ASC".format(cluster)
+			else:
+				query = "SELECT * FROM (SELECT a.PhageID, c.name, a.status " \
+						"FROM phage AS a INNER JOIN gene AS b ON a.PhageID " \
+						"= b.PhageID INNER JOIN pham AS c ON b.GeneID = " \
+						"c.GeneID WHERE a.Cluster = '{}' AND a.status = " \
+						"'final') AS d ORDER BY d.PhageID ASC".format(cluster)
+
+			con = pms.connect("localhost", username, password, database)
+			cur = con.cursor()
+			cur.execute(query)
+			results = cur.fetchall()
+			con.close()
+			for result in results:
+				phageid = result[0]
+				phams = pham_data.get(phageid, [])
+				phams.append(result[1])
+				pham_data[phageid] = phams
+			return pham_data
+		except pms.err.Error as err:
+			print(err)
+			return
+
+
+def get_phams_by_phage(username, password, database, phage):
+	"""
+	Connects to MySQL using verified username, password, and a database
+	and queries for all phages and phams in the database matching the
+	input phage.
+	:param username: verified MySQL username
+	:param password: verified MySQL password
+	:param database: selected MySQL database
+	:param phage: selected genome
+	:return pham_data: dictionary whose keys are phageids and whose
+	values are the phams in that phageid.
+	"""
 	try:
-		phams = []
-		connection = pms.connect("localhost", username, password, database)
-		cursor = connection.cursor()
-		cursor.execute("SELECT DISTINCT d.name FROM (SELECT a.PhageID, "
-					   "c.name FROM phage AS a INNER JOIN gene AS b ON "
-					   "a.PhageID = b.PhageID INNER JOIN pham AS c ON "
-					   "b.GeneID = c.GeneID) AS d WHERE d.PhageID = '{}' "
-					   "ORDER BY d.name ASC".format(phage))
-		results = cursor.fetchall()
+		pham_data = {}
+		query = "SELECT * FROM (SELECT a.PhageID, c.name, a.status FROM phage" \
+				" AS a INNER JOIN gene AS b ON a.PhageID = b.PhageID INNER " \
+				"JOIN pham AS c ON b.GeneID = c.GeneID WHERE a.PhageID = " \
+				"'{}') AS d ORDER BY d.PhageID ASC".format(phage)
+		con = pms.connect("localhost", username, password, database)
+		cur = con.cursor()
+		cur.execute(query)
+		results = cur.fetchall()
+		con.close()
 		for result in results:
-			phams.append(int(result[0]))
-		return phams
-	except pms.err.Error:
+			phageid = result[0]
+			phams = pham_data.get(phageid, [])
+			phams.append(result[1])
+			pham_data[phageid] = phams
+		return pham_data
+	except pms.err.Error as err:
+		print(err)
 		return
