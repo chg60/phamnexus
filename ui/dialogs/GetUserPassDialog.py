364fd611a6f704f372b6ebc76743b28cc8839f08
Christian Gauthier <chg60@pitt.edu>
2019-09-24 12:34:09 -0400

    font now pulled from controller; returns user/pass rather than updating controller attributes

--- ui/dialogs/GetUserPassDialog.py
+++ ui/dialogs/GetUserPassDialog.py
@@ -4,7 +4,7 @@ from tkinter import *
 class GetMySQLUserPassDialog(Toplevel):
 	def __init__(self, controller):
 		super(GetMySQLUserPassDialog, self).__init__(master=None)
-		self.title("Enter MySQL Credentials")
+		self.title("MySQL Admin Login")
 		x_offset = int(self.winfo_screenwidth() / 3)
 		y_offset = int(self.winfo_screenheight() / 3)
 		self.geometry("+{}+{}".format(x_offset, y_offset))
@@ -15,27 +15,32 @@ class GetMySQLUserPassDialog(Toplevel):
 		self.user_frame = Frame(master=self)
 		self.user_label = Label(master=self.user_frame, text="Admin "
 															 "Username: ",
-								font=("TkDefaultFont", 12))
+								font=(self.controller.font_family,
+									  self.controller.font_size))
 		self.user_label.pack(side=LEFT, anchor=W, expand=True, fill=None)
 		self.user_entry = Entry(master=self.user_frame,
-								font=("TkDefaultFont", 12))
+								font=(self.controller.font_family,
+									  self.controller.font_size))
 		self.user_entry.pack(side=LEFT, anchor=E, expand=True, fill=X)
 		self.user_frame.pack(side=TOP, fill=X)
 
 		self.pass_frame = Frame(master=self)
 		self.pass_label = Label(master=self.pass_frame, text="Admin "
 															 "Password: ",
-								font=("TkDefaultFont", 12))
+								font=(self.controller.font_family,
+									  self.controller.font_size))
 		self.pass_label.pack(side=LEFT, anchor=W, expand=True, fill=None)
 		self.pass_entry = Entry(master=self.pass_frame, show="*",
-								font=("TkDefaultFont", 12))
+								font=(self.controller.font_family,
+									  self.controller.font_size))
 		self.pass_entry.pack(side=LEFT, anchor=E, expand=True, fill=X)
 		self.pass_frame.pack(side=TOP, fill=X)
 
 		self.button_frame = Frame(master=self)
 		self.ok_button = Button(master=self.button_frame, text="Ok",
 								command=self.on_ok_click,
-								font=("TkDefaultFont", 12))
+								font=(self.controller.font_family,
+									  self.controller.font_size))
 		self.ok_button.pack(side=LEFT, anchor=E, expand=True, fill=None)
 		self.button_frame.pack(side=TOP, fill=X)
 
@@ -47,7 +52,4 @@ class GetMySQLUserPassDialog(Toplevel):
 		if password == "":
 			password = None
 		self.destroy()
-		# print(username, password)
-		self.controller.username = username
-		self.controller.password = password
-		return
+		return username, password
